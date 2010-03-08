f6d1a7396544bd00675b296f507d5f824d1e6647
Ross Hale <ross@lottay.com>
2010-03-08 10:58:40 -0800

    rh - adding test for trying to create an existing account

--- test/create_account_test.rb
+++ test/create_account_test.rb
@@ -15,10 +15,20 @@ class CreateAccountTest < Test::Unit::TestCase
 
     pp_response = @account_request.create_account(data)
 
-    p pp_response
-
     assert pp_response.success?
     assert pp_response["redirectURL"]
+  end
+
+  def test_create_account_email_address_already_an_account
+    data_filepath =  "../test/data/valid_create_account_request.json"
+
+    data = read_json_file(data_filepath)
+
+    data["emailAddress"] = "joetester@example.com"
+    pp_response = @account_request.create_account(data)
+
+    assert !pp_response.success?
+    assert_equal "An account already exists for the specified email address", pp_response["error"].first["message"]
   end
 
 end
