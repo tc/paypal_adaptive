c68365ba5d5f4115f57b4941337cc95f77aa2888
Thomas Mango <tsmango@gmail.com>
2011-10-20 19:41:59 -0400

    Refactored wrapper methods to always return PaypalAdaptive::Response objects.

--- lib/paypal_adaptive/request.rb
+++ lib/paypal_adaptive/request.rb
@@ -26,63 +26,48 @@ module PaypalAdaptive
     end
 
     def pay(data)
-      raise NoDataError unless data
-
-      response_data = call_api(data, "/AdaptivePayments/Pay")
-      PaypalAdaptive::Response.new(response_data, @env)
+      wrap_post(data, "/AdaptivePayments/Pay")
     end
 
     def set_payment_options(data)
-      raise NoDataError unless data
-
-      response_data = call_api(data, "/AdaptivePayments/SetPaymentOptions")
-      PaypalAdaptive::Response.new(response_data, @env)
+      wrap_post(data, "/AdaptivePayments/SetPaymentOptions")
     end
 
     def payment_details(data)
-      raise NoDataError unless data
-
-      call_api(data, "/AdaptivePayments/PaymentDetails")
+      wrap_post(data, "/AdaptivePayments/PaymentDetails")
     end
 
     def preapproval(data)
-      raise NoDataError unless data
-
-      response_data = call_api(data, "/AdaptivePayments/Preapproval")
-      PaypalAdaptive::Response.new(response_data, @env)
+      wrap_post(data, "/AdaptivePayments/Preapproval")
     end
 
     def preapproval_details(data)
-      raise NoDataError unless data
-
-      call_api(data, "/AdaptivePayments/PreapprovalDetails")
+      wrap_post(data, "/AdaptivePayments/PreapprovalDetails")
     end
 
     def cancel_preapproval(data)
-      raise NoDataError unless data
-
-      call_api(data, "/AdaptivePayments/CancelPreapproval")
+      wrap_post(data, "/AdaptivePayments/CancelPreapproval")
     end
 
     def convert_currency(data)
-      raise NoDataError unless data
-
-      call_api(data, "/AdaptivePayments/ConvertCurrency")
+      wrap_post(data, "/AdaptivePayments/ConvertCurrency")
     end
 
     def refund(data)
-      raise NoDataError unless data
-
-      call_api(data, "/AdaptivePayments/Refund")
+      wrap_post(data, "/AdaptivePayments/Refund")
     end
 
     def execute_payment(data)
-      raise NoDataError unless data
-
-      call_api(data, "/AdaptivePayments/ExecutePayment")
+      wrap_post(data, "/AdaptivePayments/ExecutePayment")
     end
 
-    def call_api(data, path)
+    def wrap_post(data, path)
+      raise NoDataError unless data
+
+      PaypalAdaptive::Response.new(post(data, path), @env)
+    end
+
+    def post(data, path)
       #hack fix: JSON.unparse doesn't work in Rails 2.3.5; only {}.to_json does..
       api_request_data = JSON.unparse(data) rescue data.to_json
       url = URI.parse @@api_base_url
