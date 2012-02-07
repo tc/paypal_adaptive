065b7b39aacefbfa19f79f6085662c5230cb5263
Tony La <tonyla@pingg.com>
2012-02-07 13:17:52 -0500

    Fix for using certificate based api access versus signature based.

--- lib/paypal_adaptive/ipn_notification.rb
+++ lib/paypal_adaptive/ipn_notification.rb
@@ -4,14 +4,14 @@ require 'json'
 
 module PaypalAdaptive
   class IpnNotification
-    
+
     def initialize(env=nil)
       config = PaypalAdaptive.config(env)
       @paypal_base_url = config.paypal_base_url
       @ssl_cert_path = config.ssl_cert_path
       @ssl_cert_file = config.ssl_cert_file
     end
-    
+
     def send_back(data)
       data = "cmd=_notify-validate&#{data}"
       url = URI.parse @paypal_base_url
@@ -19,17 +19,22 @@ module PaypalAdaptive
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_PEER
       http.ca_path = @ssl_cert_path unless @ssl_cert_path.nil?
-      http.ca_file = @ssl_cert_file unless @ssl_cert_file.nil?
-      
+
+      if @ssl_cert_file
+        cert = File.read(@ssl_cert_file)
+        http.cert = OpenSSL::X509::Certificate.new(cert)
+        http.key = OpenSSL::PKey::RSA.new(cert)
+      end
+
       path = "#{@paypal_base_url}/cgi-bin/webscr"
       response_data = http.post(path, data).body
-      
+
       @verified = response_data == "VERIFIED"
     end
-    
+
     def verified?
       @verified
     end
-    
+
   end
 end
