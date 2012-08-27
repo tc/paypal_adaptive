55a8ee517ab5fc622da8ba923a4db25be40a6242
Jimbo Cortes <jimbo@spuul.com>
2012-08-28 00:00:20 +0800

    added api_cert_file to config for paypal api certificate since ssl_cert_file config is used to set the CA authority.

--- lib/paypal_adaptive/ipn_notification.rb
+++ lib/paypal_adaptive/ipn_notification.rb
@@ -10,6 +10,7 @@ module PaypalAdaptive
       @paypal_base_url = config.paypal_base_url
       @ssl_cert_path = config.ssl_cert_path
       @ssl_cert_file = config.ssl_cert_file
+      @api_cert_file = config.api_cert_file
     end
 
     def send_back(data)
@@ -20,12 +21,13 @@ module PaypalAdaptive
       http.verify_mode = OpenSSL::SSL::VERIFY_PEER
       http.ca_path = @ssl_cert_path unless @ssl_cert_path.nil?
 
-      if @ssl_cert_file
-        # cert = File.read(@ssl_cert_file)
-        # http.cert = OpenSSL::X509::Certificate.new(cert)
-        # http.key = OpenSSL::PKey::RSA.new(cert)
-        http.ca_file = @ssl_cert_file
+      if @api_cert_file
+        cert = File.read(@api_cert_file)
+        http.cert = OpenSSL::X509::Certificate.new(cert)
+        http.key = OpenSSL::PKey::RSA.new(cert)
       end
+      http.ca_path = @ssl_cert_path unless @ssl_cert_path.blank?
+      http.ca_file = @ssl_cert_file unless @ssl_cert_file.blank?
 
       path = "#{@paypal_base_url}/cgi-bin/webscr"
       response_data = http.post(path, data).body
